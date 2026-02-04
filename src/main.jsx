85776656f90a1d3c147c33c1a727aed4e045add1
MukiriJoel <jmukiri535@gmail.com>
2026-02-04 15:42:24 +0300

    vercel json fix test

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeContextProvider } from './context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename={"/Landing-react"}>
+    <BrowserRouter basename={"/"}>
         <ThemeContextProvider>
             <App />
         </ThemeContextProvider>
