f8ba832f71d45a5ee42d9bdcf4f42b2a2c250674
MukiriJoel <jmukiri535@gmail.com>
2026-02-16 16:15:57 +0300

    api payload mod

--- src/store/userThunks.jsx
+++ src/store/userThunks.jsx
@@ -1,13 +1,13 @@
 import userAxiosInstance from "./userAxiosInstance";
 
 export const postUserInfo = 
-  async (name,email,company,message) => {
+  async ({name,email,company,message}) => {
     try {
       const res = await userAxiosInstance.post("/contact/user/katibu",{
-        name:name,
-        email:email,
-        company:company,
-        message:message
+        name,
+        email,
+        company,
+        message
       });
     console.log("userres",res)
       return res?.data?.body;
